6e768da55c41cd3db8d56d90ea6f4f00e4421968
Bart Riemens <briemens@crafity.com>
2011-10-18 23:55:01 +0200

    * Added an animation between the top pages (news, hello, etc)

--- js/mylibs/brandfriends.js
+++ js/mylibs/brandfriends.js
@@ -1,13 +1,27 @@
+// Remove the id's from the pages
+//  so page does not scroll down
+//  when a link is clicked.
+//  From this point on the navigation
+//  is controlled by JavaScript.
+$(".page").attr("id", null);
+
 window.onhashchange = function () {
 	var location = window.location.hash.substr(1) || "news";
 	console.log(location);
-	$(".page").addClass("hidden");
+	//$(".page").addClass("hidden");
 	var page$ = $("." + location + ".page");
-	
+
 	if (!page$.length) {
 		page$ = $(".news.page");
 	}
+
+	var pageTop = page$.position().top;
+	console.log("pageTop", pageTop);
+	$(".pages").css("top", pageTop * -1);
+	
 	page$.removeClass("hidden");
+
+	window.scrollTo(0, 0);
 };
 
 window.onhashchange();
