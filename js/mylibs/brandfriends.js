09ca6e37c15ee6beb015fdcef4caef2d43512eee
Bart Riemens <briemens@crafity.com>
2011-10-19 03:18:23 +0200

    Basic implementation of the bottom part of the screen (portfolio). Buttons are working, but a lot of small tweaking left.

--- js/mylibs/brandfriends.js
+++ js/mylibs/brandfriends.js
@@ -9,15 +9,45 @@ window.onhashchange = function () {
 	var location = window.location.hash.substr(1) || "news";
 	console.log(location);
 	//$(".page").addClass("hidden");
-	var page$ = $("." + location + ".page");
+	if (location === "page1") {
+		$("#bottom .pages").css("left", 0);
+		return;
+	} else if (location === "page2") {
+		$("#bottom .pages").css("left", "-100%");
+		return;
+	}
+	console.log($(".projects").position().top);
+	if (location === "previousproject") {
+		$("#bottom .pages").css("left", 0);
+		if ($(".projects").position().top < 0) {
+			$(".projects").css("top", 0);
+		} else {
+			$(".projects").css("top", -$(".projects").height());
+		}
+		window.location.hash = "#done";
+		return;
+	} else if (location === "nextproject") {
+		$("#bottom .pages").css("left", 0);
+		if ($(".projects").position().top < 0) {
+			$(".projects").css("top", 0);
+		} else {
+			$(".projects").css("top", -$(".projects").height());
+		}
+		window.location.hash = "#done";
+		return;
+	}
+
+
+
+	var page$ = $("#top ." + location + ".page");
 
 	if (!page$.length) {
-		page$ = $(".news.page");
+		page$ = $("#top .news.page");
 	}
 
 	var pageTop = page$.position().top;
 	console.log("pageTop", pageTop);
-	$(".pages").css("top", pageTop * -1);
+	$("#top .pages").css("top", pageTop * -1);
 	
 	page$.removeClass("hidden");
 
