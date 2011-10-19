c3d66c3ff1eb5e4ea5f86f7a61a86d9b49b5511e
Galina Slavova <galina@crafity.com>
2011-10-19 18:31:35 +0200

    1. Refined the button layouts in the portfolio section. 2. Set the current page with a background red. 3. Programmed - whe swich to next/previous project is done, then set the first page to be the current one (red). 4. Sliced the game_nuts image and put it into page.

--- js/mylibs/brandfriends.js
+++ js/mylibs/brandfriends.js
@@ -7,38 +7,92 @@ $(".page").attr("id", null);
 
 window.onhashchange = function () {
 	var location = window.location.hash.substr(1) || "news";
-	console.log(location);
-	//$(".page").addClass("hidden");
-	if (location === "page1") {
-		$("#bottom .pages").css("left", 0);
-		return;
-	} else if (location === "page2") {
-		$("#bottom .pages").css("left", "-100%");
-		return;
-	}
-	console.log($(".projects").position().top);
-	if (location === "previousproject") {
-		$("#bottom .pages").css("left", 0);
-		if ($(".projects").position().top < 0) {
-			$(".projects").css("top", 0);
-		} else {
-			$(".projects").css("top", -$(".projects").height());
-		}
-		window.location.hash = "#done";
-		return;
-	} else if (location === "nextproject") {
-		$("#bottom .pages").css("left", 0);
-		if ($(".projects").position().top < 0) {
-			$(".projects").css("top", 0);
-		} else {
-			$(".projects").css("top", -$(".projects").height());
-		}
-		window.location.hash = "#done";
+	console.log("location: " + location);
+
+	if (location.indexOf("page") === 0) {
+		// reset the current page in projects section in the beginning
+		$(".button").removeClass("current");
+
+		var pageNumber = location.substr(4);
+
+		$(".button").each(function () {
+			if ($(this).text() === pageNumber) {
+
+				//console.log("Targeted button: " + pageNumber);
+				$(this).addClass("current");
+			}
+		});
+
+		var number = parseInt(pageNumber) - 1;
+		$("#bottom .pages").css("left", (number * -100) + "%");
 		return;
 	}
 
+	console.log(".peojects.position.top: " + $(".projects").position().top);
 
+	var indexOfProject = location.indexOf("project");
+	console.log("indexOfProject: " + indexOfProject);
+	if (indexOfProject > 0) {
 
+		var prevNextProject = location.substr(0, indexOfProject);
+
+		// set this project to its first (leftmost) page
+		$("#bottom .pages").css("left", 0);
+
+		if ($(".projects").position().top < 0) {
+			$(".projects").css("top", 0);
+			$(".projects .project").removeClass("current").first().addClass("current");
+			$(".button").removeClass("current");
+
+			$(".projects .project.current .button").each(function () {
+				if ($(this).text() === "1") {
+					console.log("Add class current to button!");
+					$(this).addClass("current");
+				}
+			});
+		} else {
+			$(".projects").css("top", -$(".projects").height());
+			$(".projects .project").removeClass("current").last().addClass("current");
+			$(".button").removeClass("current");
+
+			$(".projects .project.current .button").each(function () {
+				if ($(this).text() === "1") {
+					console.log("Add class current to button!");
+					$(this).addClass("current");
+				}
+			});
+		}
+
+		window.location.hash = "#done";
+		return;
+	}
+
+	/*
+	 if (location === "previousproject") {
+
+	 $("#bottom .pages").css("left", 0);
+
+	 if ($(".projects").position().top < 0) {
+	 $(".projects").css("top", 0);
+	 } else {
+	 $(".projects").css("top", -$(".projects").height());
+	 }
+
+	 window.location.hash = "#done";
+	 return;
+
+	 } else if (location === "nextproject") {
+
+	 $("#bottom .pages").css("left", 0);
+	 if ($(".projects").position().top < 0) {
+	 $(".projects").css("top", 0);
+	 } else {
+	 $(".projects").css("top", -$(".projects").height());
+	 }
+	 window.location.hash = "#done";
+	 return;
+	 }
+	 */
 	var page$ = $("#top ." + location + ".page");
 
 	if (!page$.length) {
@@ -48,7 +102,7 @@ window.onhashchange = function () {
 	var pageTop = page$.position().top;
 	console.log("pageTop", pageTop);
 	$("#top .pages").css("top", pageTop * -1);
-	
+
 	page$.removeClass("hidden");
 
 	window.scrollTo(0, 0);
